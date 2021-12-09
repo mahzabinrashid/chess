a93123a3477447a47e3139194fa1b591b506c1ab
Sadman Ahmed <sadmanahmed006@gmail.com>
2021-12-09 02:47:55 -0500

    wip-ai

---
+++ computer.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include "computer.h"
+
+using namespace std;
+
+void level_1(Board &b) {
+    vector<shared_ptr<Piece>> pieces;
+    
+}
---
+++ computer.h
@@ -0,0 +1,15 @@
+#ifndef _COMPUTER_H_
+#define _COMPUTER_H_
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <memory>
+#include "player.h"
+
+void level_1(Board &b); //more variables might be needed
+void level_2(Board &b); // ,,
+void level_3(Board &b); // ,,
+void level_4(Board &b); // ,,
+
+#endif
