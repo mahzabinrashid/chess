0b06d9b564edb4d0de1064ce78a8ae16856ae88b
Mahzabin Rashid Fariha <mahzabinrashidfariha@gmail.com>
2021-12-10 01:57:25 -0500

    further changes

--- human.cc
+++ human.cc
@@ -3,13 +3,13 @@
 
 using namespace std;
 
-Human::Human(bool white_player, bool human): Student{white_player, human} {
+Human::Human(bool white_player, bool human): Player{white_player, true} {
 }
 
- bool Human::is_white_player() {
+bool Human::is_white_player() {
      return white_player;
- };
+};
 
 bool Human::is_human() {
-    return human;
+    return true;
 };  
