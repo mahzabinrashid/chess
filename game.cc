36ccbe4669764b4b3cb014a99cd72c088880f5bd
Sadman Ahmed <sadmanahmed006@gmail.com>
2021-12-11 02:30:38 -0500

    game update

--- game.cc
+++ game.cc
@@ -6,7 +6,6 @@ using namespace std;
 Game::init(Player p1, Player p2) {
     players.emplace_back(p1);
     players.emplace_back(p2);
-    board.create_board();
     if (p1.is_white_player()) {
         current_player_turn = p1;
     } else {
@@ -14,17 +13,15 @@ Game::init(Player p1, Player p2) {
     }
 };
 
-Game::player_move(Player player, int col_i, int row_i, int col_f, int row_f) {
-    Piece* source_piece = board[row_i][col_i].get_piece();
-    Piece* dest_piece = board[row_f][col_f].get_piece();
+Game::player_move(Player player) {
     if (player != current_player_turn) { //not that player's turn
         cout << "It's not your turn" << endl;
         return false;
     } else {
-        if (this->current_player_turn == players[0]) {
-            this->current_player_turn = players[1];
+        if (current_player_turn == players[0]) {
+            current_player_turn = players[1];
         } else {
-            this->current_player_turn = players[0];
+            current_player_turn = players[0];
         }
         return true;
     }
--- game.h
+++ game.h
@@ -1,14 +1,11 @@
 #ifndef _GAME_
 #define _GAME_
-#include "board.h"
 #include "player.h"
-#include "move.h"
 #include <vector>
 #include <memory>
 
 class Game {
     vector<Player> players;
-    Board board;
     Player current_player_turn;
     void init(Player p1, Player p2);
     public:
